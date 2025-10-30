62f761020db278201e2176d5a676ea0ade93d55f
KyraWynne9 <kewynne@email.neit.edu>
2025-10-30 10:31:27 -0400

    a

--- challenges.js
+++ challenges.js
@@ -1,2 +1,12 @@
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d");
+
+
+function gameloop() {
+//draw background
+//draw stars
+//move stars
+//recycle stars/restart stars
+}
+
+setInterval(gameloop, 50);
